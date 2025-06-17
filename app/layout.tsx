6c96e6d5a0104c4e876ec0be65352cc5be89b7a3
sam <sanskarsharma9005@gmail.com>
2025-06-17 18:37:02 +0530

    add Vercel Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { WalletContextProvider } from '@/components/providers/wallet-provider'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'PikaVault - Digital Card Collection',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <meta property="og:url" content="https://x.com"></meta>
       <body>
         <WalletContextProvider>{children}</WalletContextProvider>
+        <Analytics/>
       </body>
     </html>
   )
